8d2c81d12808f4990bf647968159edee108e7511
Igor Goroshko <i.s.goroshko@gmail.com>
2022-11-15 19:06:23 +0700

    fixed issue 27: Backend should return proper error message in the API response

--- src/errors/helper.js
+++ src/errors/helper.js
@@ -1,12 +1,12 @@
-const {NotFoundError, ForbiddenError} = require("./custom-errors");
+const { NotFoundError, ForbiddenError } = require("./custom-errors");
 
-function handleErrors(res, error, defaultMessage = 'Internal error...') {
-    if(error instanceof NotFoundError){
-        res.status(404).json({message: error.message})
-    }else if(error instanceof ForbiddenError){
-        res.status(403).json({message: error.message})
-    }else{
-        res.status(500).json({message: "Internal Error..."})
+function handleErrors(res, error) {
+    if (error instanceof NotFoundError) {
+        res.status(404).json({ message: error.message })
+    } else if (error instanceof ForbiddenError) {
+        res.status(403).json({ message: error.message })
+    } else {
+        res.status(500).json({ message: `Unexpected error, see details: ${error.message}` })
     }
     console.log(error.message)
 }
